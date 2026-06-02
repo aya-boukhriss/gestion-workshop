f791a8b737685dd4cc8127829074d6d786932abd
aya boukhriss <ayaboukhriss128@gmail.com>
2026-06-02 21:39:49 +0100

    Ajout catégories et notes visibles

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'role'
+        'role',
+        'photo',
+        'telephone',
+        'bio'
     ];
 
     protected $hidden = [
@@ -30,37 +33,31 @@ class User extends Authenticatable
         ];
     }
 
-    // Vérifier si l'utilisateur est admin
     public function isAdmin()
     {
         return $this->role === 'admin';
     }
 
-    // Vérifier si l'utilisateur est formateur
     public function isFormateur()
     {
         return $this->role === 'formateur';
     }
 
-    // Vérifier si l'utilisateur est manager
     public function isManager()
     {
         return $this->role === 'manager';
     }
 
-    // Un user a plusieurs inscriptions
     public function inscriptions()
     {
         return $this->hasMany(Inscription::class, 'id_participant');
     }
 
-    // Un user formateur a plusieurs workshops
     public function workshops()
     {
         return $this->hasMany(Workshop::class, 'id_formateur');
     }
 
-    // Un user formateur a un profil formateur
     public function formateur()
     {
         return $this->hasOne(Formateur::class, 'id_utilisateur');
